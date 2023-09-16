bd279f8cac84da8044c2d6360fbd055fea505ff7
timmytruong <timmy7truong@gmail.com>
2023-09-16 17:17:44 -0400

    Update dependencies and organize into kt files
    Signed-off-by: timmytruong <timmy7truong@gmail.com>

---
+++ buildSrc/src/main/java/Config.kt
@@ -0,0 +1,11 @@
+object Config {
+    const val APP_ID = "com.timmytruong.habit_launcher"
+    const val VERSION_NAME = "1.0"
+    const val VERSION_CODE = 1
+
+    object SdkVersions {
+        const val COMPILE = 33
+        const val MIN = 24
+        const val TARGET = 33
+    }
+}
